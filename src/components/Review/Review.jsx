b8e146e371463245617c4190f998046f4f6f5dbc
Brandon Wiedemeier <brandon.wiedemeier@gmail.com>
2019-08-24 11:54:47 -0500

    reducers are rendering to the DOM correctly.  Additional functions created, debating on utilizing as I progress

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -4,15 +4,41 @@ import axios from 'axios';
 
 class Review extends Component {
 
+    //create local state to take in reducers info
+    state = {
+        item : {
+            feelings: this.props.store.feelingsReducer,
+            understanding: this.props.store.understandingReducer,
+            supported: this.props.store.supportedReducer,
+            comments: this.props.store.commentsReducer,
+        }
+    }
+    //get info to display on page load
+    componentDidMount() {
+        // this.feedbackReview();
+        // this.createPizzaOrder();  
+    }
+
     handleClick = () => {
         //pushes us to Collection page
         this.props.history.push('/success')
     }
 
-    //pizza-parlor is a decent example
-    //create local state to take in reducers info
     //create function to set local state with reducers info
-    //display items on the DOM
+    // feedbackReview = () => {
+    //     console.log(this.state);
+    //     this.setState({
+    //         item: {
+    //             feelings: this.props.store.feelingsReducer,
+    //             understanding: this.props.store.understandingReducer,
+    //             supported: this.props.store.supportedReducer,
+    //             comments: this.props.store.commentsReducer,
+    //         }
+    //     });
+    // };
+
+    //pizza-parlor is a decent example
+    
     //create POST to get data to database, and .history to move to success page
     //POST needed with axios to get data to database
     //POST in server side
@@ -22,6 +48,17 @@ class Review extends Component {
         return (
             <>
             <h2>Review Your Feedback</h2>
+            {/* display items on the DOM */}
+            <div>
+                
+                {this.state.item.feelings}
+                <br/>
+                {this.state.item.understanding}
+                <br/>
+                {this.state.item.supported}
+                <br/>
+                {this.state.item.comments}
+            </div>
             <button onClick={this.handleClick}>SUBMIT</button>
             </>
         )
